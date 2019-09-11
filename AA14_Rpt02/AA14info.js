098419f6910c9bf6d08da14dfa62d30b3d140f41
qzaq5985 <54881065+qzaq5985@users.noreply.github.com>
2019-09-11 16:55:01 +0900

    Add files via upload

---
+++ AA14_Rpt02/AA14info.js
@@ -0,0 +1,8 @@
+// AA14info.js
+
+module.exports = function(id, name, phone) {
+	console.log("My info");
+	console.log("ID : " + id);
+	console.log("Name : " + name);
+	console.log("Phone : " + phone + "\n");
+}
---
+++ AA14_Rpt02/index_AA14.js
@@ -0,0 +1,7 @@
+// index_AA14.js
+
+var myinfo = require('./AA14info');
+
+myinfo("AA14", "윤지훈", "010-1234-5678");
+
+myinfo("AA14", "YJH", "010-2345-6789");
